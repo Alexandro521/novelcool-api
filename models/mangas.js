332b50b9cb8973508835eea1b656d4d872974077
Alexandro Mendez <elavion0151@gmail.com>
2024-03-25 23:08:00 -0400

    refactorizando funcion getMangas

--- models/mangas.js
+++ models/mangas.js
@@ -12,6 +12,43 @@ async function AxiosGet(url){
     return await axios.get(url,axiosHead)
 }
 
+function getMangas($,Element,parent=""){
+    const mangaList = []
+    $(Element).find(parent+'> .book-item').each((mangaIndex,mangaElement)=>{
+        const genresList = [];
+        let regex = /\/([^\/]+)\.html$/;
+        //elemento padre comun de todos los elementos
+        const index = $(mangaElement).find('.book-info');
+        const id = $(mangaElement).find('.book-pic a').attr('href').match(regex)
+        const title = index.find('.book-name').text();
+        const img = $(mangaElement).find('.book-pic a img').attr('cover_url');
+        const typeManga = $(mangaElement).find('.book-pic a').find('.book-type-manga').text();
+        $(mangaElement).find('.book-pic a').find('.book-data-info .book-tags > .book-tag').each((genreIndex,genreElement)=>{
+            const genre = $(genreElement).text()
+            genresList.push(genre)
+        });
+        const bookRate = index.find('.book-rate .book-rate-num').text();
+        const description = index.find('.book-intro').text();
+        const views = index.find('.book-data .book-data-item').find('.book-data-num').text();
+        const dataTime = index.find('.book-data .book-data-item').find('.book-data-time').text();
+
+
+        const mangaObject = {
+            id:[...id][1],
+            title: title,
+            img: img,
+            typeManga:typeManga,
+            genres: [...genresList],
+            bookRate:bookRate,
+            description:description,
+            views:views,
+            dataTime:dataTime
+        }
+        mangaList.push(mangaObject)
+    })
+    return mangaList
+}
+
  export class Mangas{
 
     static async get_mangasIndex(){
@@ -20,41 +57,10 @@ async function AxiosGet(url){
         const sectionList = []
         //obtener todas la secciones principales con sus repectivos mangas
         $('.index-book-list').each((index,Element)=>{
-
+        
             const nameSection = $(Element).find('.site-content a ').attr('title');
-            const mangaList = []
-            $(Element).find('.book-list > .book-item').each((mangaIndex,mangaElement)=>{
-                const genresList = [];
-                let regex = /\/([^\/]+)\.html$/;
-                //elemento padre comun de todos los elementos
-                const index = $(mangaElement).find('.book-info');
-                const id = $(mangaElement).find('.book-pic a').attr('href').match(regex)
-                const title = index.find('.book-name').text();
-                const img = $(mangaElement).find('.book-pic a img').attr('cover_url');
-                const typeManga = $(mangaElement).find('.book-pic a').find('.book-type-manga').text();
-                $(mangaElement).find('.book-pic a').find('.book-data-info .book-tags > .book-tag').each((genreIndex,genreElement)=>{
-                    const genre = $(genreElement).text()
-                    genresList.push(genre)
-                });
-                const bookRate = index.find('.book-rate .book-rate-num').text();
-                const description = index.find('.book-intro').text();
-                const views = index.find('.book-data .book-data-item').find('.book-data-num').text();
-                const dataTime = index.find('.book-data .book-data-item').find('.book-data-time').text();
 
-
-                const mangaObject = {
-                    id:[...id][1],
-                    title: title,
-                    img: img,
-                    typeManga:typeManga,
-                    genres: [...genresList],
-                    bookRate:bookRate,
-                    description:description,
-                    views:views,
-                    dataTime:dataTime
-                }
-                mangaList.push(mangaObject)
-            })
+            const mangaList = getMangas($,Element,'.book-list');
 
             const sectionObject = {
                 nameSection:nameSection,
@@ -65,6 +71,8 @@ async function AxiosGet(url){
         })
         return sectionList
     }
+
+
  }
  const result = await Mangas.get_mangasIndex()
- console.log(result[0]);
+ console.log(result[1]);
