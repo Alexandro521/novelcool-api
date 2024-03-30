c726a405d95333320765bce4ce9f542d37fb5375
Alexandro Mendez <elavion0151@gmail.com>
2024-03-30 17:58:23 -0400

    listo

--- models/mangas.js
+++ models/mangas.js
@@ -1,6 +1,6 @@
 import cheerio from 'cheerio';
 import axios from 'axios';
-import { getCategoryList } from './modules/categoryList.js';
+import { getCategoryList,filterCategoryList} from './modules/categoryList.js';
 import { pagination } from './modules/pagination.js';
 import { getMangas } from './modules/getMangas.js';
 
@@ -136,7 +136,7 @@ async function get_category(categoryUrl){
        }    
 
        return {
-        mangaInfo:{
+        
             img: img,
             title:title,
             bookType:bookType,
@@ -147,7 +147,7 @@ async function get_category(categoryUrl){
             description:description,
             genres:genres,
             chaptersList: chaptersList
-        }}
+        }
     }
     static async chapter_read(url){
 
@@ -175,15 +175,80 @@ async function get_category(categoryUrl){
             })
         }
        // console.log(imgList)
-        console.log(imgList.length)
+   
         return {
             captitle:title,
             prevChapter:prevChapter,
             nextChapter:nextChapter,
             pages: imgList
         }
- }
+    }
+    static async search(
+
+    searchText,
+    Author='',
+    textSearchMethod='contain',
+    autorSearchMehod='contain',
+    categories_Include = '',
+    categories_not_Include = '',
+    publishYear='',
+    complete_Series='',
+    rate_star= ''
+    ){
+    
+
+   let SearchParams = {
+        textSearchMethodOptions: ['contain','begin','End'],
+        autorSearchMehodOptions: ['contain','begin','End'],
+        searchText: searchText ?? 'none',
+        Author: Author ?? 'none',
+        categories_Include: categories_Include ?? 'none',
+        categories_not_Includes: categories_not_Include ?? 'none',
+        publishYear: publishYear ?? 'none',
+        complete_Series: complete_Series ??'none',
+        rate_star: rate_star ?? 'none'
+    }
+    const categoriesIncludes = '%2C'+ [categories_Include].join('%2C')
+    const categoriesNotIncludes = '%2C'+ [categories_not_Include].join('%2C')
+    console.log(categoriesIncludes)
+
+  const searchUrl =   `${mainUrl()}search/?name_sel=${textSearchMethod}&name=${searchText}&author_sel=${autorSearchMehod}&author=${Author}&category_id=${categoriesIncludes}&out_category_id=${categoriesNotIncludes}&publish_year=${publishYear}&completed_series=${complete_Series}&rate_star=${rate_star}`;
+
+ console.log(searchUrl)
+  console.log("-------------------")
+  const URL =searchUrl;
+  const response = await AxiosGet(searchUrl);
+  const $ = cheerio.load(response.data);
+  //const categoryTitle = $('.category-headline-bar .category-title').text()
+  const mangaList = []
+  $('.site-content').find('.category-book-list').find('.book-item').each((mangaIndex,mangaElement)=>{
+      const result = getMangas($,mangaElement);
+      mangaList.push(result)
+  })
+ // const page = pagination($)
+  const categories = filterCategoryList($)
+  //nota:utilizar memorizacion para esto
+  
+  return {
+    head:{
+      filter: SearchParams,
+      categoryGroup: categories,
+
+    },
+    body:{
+      result: mangaList,
+      //pagination: page
+    }
+  }
+    }
 
 }
 
+// const result = await Mangas.search('circles')
+// const result2 = await Mangas.get_info('Circles')
+// const result3 = await Mangas.chapter_read('Cap-tulo-154/11858129/')
+// console.log(result.body.result[0])
+
+// console.log(result2.chaptersList[0])
+// console.log(result3)
 
