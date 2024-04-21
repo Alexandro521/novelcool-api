8c5402fe012230e26842deb6366d262224bfa464
Alexandro mendez <133552055+Alexandro521@users.noreply.github.com>
2024-04-21 11:41:06 -0400

    Update index.js

--- index.js
+++ index.js
@@ -3,13 +3,28 @@ import { mangasRouter } from './router/mangas.js';
 import cors from 'cors'
 const app = express()
 const PORT = process.env.PORT ?? 1234;
-const corsOption = {
-    origin: ['http://localhost:3000/','http://127.0.0.1:3001/'],
-    methods:['GET']
-}
-app.use(cors(corsOption))
+
+app.use(cors())
 app.use('/api',mangasRouter)
- 
+
+ app.get('*',(req,res)=>{
+    res.json({
+        message: 'path not exist',
+        initPath:'/api',
+        path: {
+           indexPage: '/index',
+           lastRelease:'/lasted',
+           completeBooks: '/complete',
+           categories:['/romance', '/comedy','/drama','/action','/webcomic'],
+           anyCategory: '/category/:category',
+           search: '/search',
+           getInfoById:'/view/:id',
+           getGenresList:'/info/genres',
+           getMangaChapterPages:'/reader/:ch/:id',
+
+        }
+    })
+})
 app.listen(PORT,()=>{
     console.log('servidor en Linea en el puerto : '+ PORT)
 })
